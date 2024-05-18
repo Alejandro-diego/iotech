35ced0e1bb17dc7b2393ed96b5fe80366aad9ac6
alemax75 <ale_max2015@yahoo.com.br>
2024-05-17 23:15:36 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'IOTECH',
+      title: 'IOTech',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 184, 193, 224)),
